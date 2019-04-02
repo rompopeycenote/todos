cb82acfb34f9a4e460c871a426fd54f13a819779
rompopeycenote <49194277+rompopeycenote@users.noreply.github.com>
2019-04-02 13:36:10 +0200

    Add files via upload

---
+++ dist/js/about.f90264ad.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,s){"use strict";s.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"about"},[s("h1",[t._v("About")]),s("p",[t._v("This is the TodoList app v1.0.0 It is part of the Vue Crash Course")])])}],u=s("2877"),o={},r=Object(u["a"])(o,n,a,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.f90264ad.js.map
